5b93c4b7ba937ecf168518468d78ec0aca0aed74
T U <tylerletendre98@gmail.com>
2021-05-03 21:02:44 -0400

    deleted my flashcards.js file so i now only have a collections.js file with routes

--- index.js
+++ index.js
@@ -3,14 +3,12 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const collections = require('./routes/collections');
-const flashcards = require('./routes/flashcards');
 
 
 connectDB();
 app.use(cors());
 app.use(express.json());
 app.use('/api/collections', collections);
-app.use('/api/flashcards', flashcards);
 
 
 const port = process.env.PORT || 5000;
